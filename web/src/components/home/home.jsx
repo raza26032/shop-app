33f09d39f82c3e984fee3d14d88c3a9f9e5a105d
jahanzaibsheikh830 <jahanzaibsheikh830gmail.com>
2021-03-08 17:33:48 +0500

    some-changes

--- web/src/components/home/home.jsx
+++ web/src/components/home/home.jsx
@@ -1,61 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios"
 
 function Home() {
-  const [products, setproducts] = useState([
-    {
-      id: '1',
-      title: 'Chocolate Cake',
-      price: 1500,
-      image: 'https://www.dilpasandsweets.com/wp-content/uploads/2019/07/img23-3.png',
-    },
-    {
-      id: '2',
-      title: 'Valentine Cake',
-      price: 980,
-      image: 'https://www.dilpasandsweets.com/wp-content/uploads/2019/07/img19-3.png',
-    },
-    {
-      id: '3',
-      title: 'Chocolate Cake',
-      price: 980,
-      image: 'https://www.dilpasandsweets.com/wp-content/uploads/2019/07/img16-4.png',
-    },
-    {
-      id: '4',
-      title: 'Cricket Cake',
-      price: 980,
-      image: 'https://www.dilpasandsweets.com/wp-content/uploads/2019/07/img17-4.png',
-    },
-    {
-      id: '5',
-      title: 'Chocolate Cake',
-      price: 1500,
-      image: 'https://www.dilpasandsweets.com/wp-content/uploads/2019/07/img23-3.png',
-    },
-    {
-      id: '6',
-      title: 'Valentine Cake',
-      price: 980,
-      image: 'https://www.dilpasandsweets.com/wp-content/uploads/2019/07/img19-3.png',
-    },
-    {
-      id: '7',
-      title: 'Chocolate Cake',
-      price: 980,
-      image: 'https://www.dilpasandsweets.com/wp-content/uploads/2019/07/img16-4.png',
-    },
-    {
-      id: '8',
-      title: 'Cricket Cake',
-      price: 980,
-      image: 'https://www.dilpasandsweets.com/wp-content/uploads/2019/07/img17-4.png',
-    },
-  ])
+  const [products, setproducts] = useState([])
 
   const [carts, setcarts] = useState([])
   console.log(carts)
   function addcart(product) {
-    var result = carts.find((carts) => carts.id === product.id);
+    var result = carts.find((carts) => carts._id === product._id);
 
     if (result === undefined) {
       setcarts([...carts, product])
@@ -63,18 +15,31 @@ function Home() {
       alert("Product has Already Added")
     }
   }
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://localhost:5000/getProducts',
+      withCredentials: true
+    }).then((response) => {
+      // console.log(response.data.data)
+      setproducts(response.data.data)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
   return (
-    <div style={{width: "100%"}}>
+    <div style={{ width: "100%" }}>
       <img class="alignnone wp-image-13549 size-full" src="https://rehmateshereen.com/wp-content/uploads/2020/01/banner-Rshree-Hyderabadi-coffee-cake.jpg" alt="" width="1348" height="585" />
 
       <hr />
-      <div className="card1" style={{width: "100%"}}>
+      <div className="card1" style={{ width: "100%" }}>
         {products.map((product, index) => (
           <div className="eachproduct" key={index}>
-            <img className="pic" src={product.image} alt={product.title} />
-            <h1>{product.title}</h1>
+            <img className="pic" src={product.image} alt={product.name} />
+            <h1>{product.name}</h1>
             <br />
-            <div className="price">PKR: {product.price}/- Per kg</div>
+            <div className="price">PKR: <b>{product.price}</b>/- Per kg</div>
             <div>
               <br />
               <button onClick={() => addcart(product)} >Add To Cart</button>
@@ -97,7 +62,7 @@ function Home() {
       <hr />
       <br />
       <div className="footer">
-        <p className="textcopy">Copyright © 2021 All Rights Reserved by <b>Ajx Solutions</b>.</p>
+        <p className="textcopy">Copyright © 2021 All Rights Reserved by <b>AR Solutions</b>.</p>
       </div>
     </div>
   )
